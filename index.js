1989f54046fb72a21cfd7923efbb38c12afcc8cf
Small Guo <34023615+CarSmallGuo@users.noreply.github.com>
2019-07-17 13:59:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -107,13 +107,13 @@ const ScrollableTabView = createReactClass({
     };
   },
 
-  componentWillReceiveProps(props) {
-    if (props.children !== this.props.children) {
-      this.updateSceneKeys({ page: this.state.currentPage, children: props.children, });
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.children !== this.props.children) {
+      this.updateSceneKeys({ page: this.state.currentPage, children: this.props.children, });
     }
 
-    if (props.page >= 0 && props.page !== this.state.currentPage) {
-      this.goToPage(props.page);
+    if (this.props.page >= 0 && prevProps.page !== this.state.currentPage) {
+      this.goToPage(this.props.page);
     }
   },
 
